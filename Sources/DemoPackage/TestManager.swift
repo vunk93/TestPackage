b45a7ac7e7298b857e7689702b4e923add8df442
vunk <vunk@apero.vn>
2022-09-24 15:37:26 +0700

    init

---
+++ Sources/DemoPackage/TestManager.swift
@@ -0,0 +1,16 @@
+//
+//  File.swift
+//  DemoPackage
+//
+//  Created by ANH VU on 24/09/2022.
+//
+
+import Foundation
+
+public class TestManager {
+    static public let shared = TestManager()
+    
+    func log() {
+        print("connected")
+    }
+}
